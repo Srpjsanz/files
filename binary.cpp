88d9cd6bea7b006821fbe33dce2f437a22980e46
srpjsanz <srpjsanz@users.noreply.github.com>
2018-02-24 20:46:15 -0400

    Added header file

---
+++ binary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 	
+   ofstream file;
+   file.open ("abc.bin");
+   file<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   file.close(); 
+   return 0;
+}
